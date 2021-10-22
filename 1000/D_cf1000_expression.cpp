cf812d3f0accc621d92a5f925792f8574e76a7cc
Jerry3T <jeremygarciabwhs@gmail.com>
2021-10-22 16:08:13 -0400

    pain

---
+++ 1000/D_cf1000_expression.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+
+using namespace std;
+
+int count = 0;
+
+int test(int a, int b, int c) {
+
+    int num = 0;
+
+    if (b == 1) {
+
+        if (b != c && b != a && a <= c) {
+
+            num = (a+b) * c;
+
+        } else if (b != c && b != a && c <= a) {
+
+            num = a * (b+c);
+
+        } else if (c == 1 && a > 1) {
+            
+            num = a * (b+c);
+
+        } else num = a + b + c;
+        
+        count += 1;
+
+    }
+
+    if (a == 1) {
+
+        if (a != c) {
+
+            num = (a+b) * c;
+
+        } else num = a + b + c;
+
+        count+=1;
+
+    }
+
+    return num;
+
+}
+
+int main() {
+
+    int a;
+    int b;
+    int c;
+
+    cin >> a >> b >> c;
+
+    int test1 = test(a, b, c);
+    int test2 = test(c, b, a);
+
+    if (test1 > 0) cout << test1;
+    if (test2 > 0 && test1 == 0) cout << test2;
+    if (count == 0) cout << a * b * c;
+
+    return 0;
+
+}
